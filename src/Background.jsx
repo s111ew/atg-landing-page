333560dbd69b8cfac19f7c2842d0ee13287e6928
s111ew <167631312+s111ew@users.noreply.github.com>
2024-12-17 22:19:35 +0000

    Add extra carousel

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,20 +1,14 @@
 import ImageRow from "./imageRow.jsx"
-import img1 from "./assets/images/carousel-1/carousel-row-1-1.png"
-import img2 from "./assets/images/carousel-1/carousel-row-1-2.png"
-import img3 from "./assets/images/carousel-1/carousel-row-1.png"
-import img4 from "./assets/images/carousel-1/carousel-row-1(1).png"
-import img5 from "./assets/images/carousel-1/carousel-row-1(2).png"
-import img6 from "./assets/images/carousel-1/carousel-row-1(3).png"
-import img7 from "./assets/images/carousel-1/carousel-row-1(4).png"
-
-const images = [img1, img2, img3, img4, img5, img6, img7]
+import { images } from "./imageModule.js"
+import { imagesTwo } from "./imageModule.js"
+import { imagesThree } from "./imageModule.js"
 
 function Background() {
   return (
     <div className="background">
       <ImageRow images={images}/>
-      <ImageRow images={images}/>
-      <ImageRow images={images}/>
+      <ImageRow images={imagesTwo}/>
+      <ImageRow images={imagesThree}/>
     </div>
   )
 }
