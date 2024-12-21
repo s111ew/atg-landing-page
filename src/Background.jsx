cbd0dd5700dfaaba012f8743cc0af7aeeda0aaab
s111ew <167631312+s111ew@users.noreply.github.com>
2024-12-21 00:30:19 +0000

    Add code snippets and scrolling animation

--- src/Background.jsx
+++ src/Background.jsx
@@ -8,11 +8,11 @@ import folderBack from "./assets/images/png/folderBack.png"
 import folderFront from "./assets/images/png/folderFront.png"
 
 const images = [
-  { key: 1, src: dog, alt: "", rotation: -6, translate: -50}, 
-  { key: 2, src: dunes, alt: "", rotation: 10, translate: -20}, 
-  { key: 3, src: cactus, alt: "", rotation: -4, translate: 40}, 
-  { key: 4, src: climbing, alt: "", rotation: 8, translate: -10}, 
-  { key: 5, src: mountain, alt: "", rotation: -12, translate: 50}, 
+  { key: 1, src: dog, srcString: "/src/assets/Dog.png", alt: "A golden labrador looks at the camera with a backdrop of an American flag", rotation: -6, translate: -60, rotationSnippet: 3, translateSnippet: 35}, 
+  { key: 2, src: dunes, srcString: "/src/assets/Dunes.png", alt: "Rolling sand dunes with a backdrop of a light blue sky", rotation: 10, translate: -40, rotationSnippet: -2, translateSnippet: 40}, 
+  { key: 3, src: cactus, srcString: "/src/assets/Cactus.png", alt: "A collection of succulents and small cactuses in teracotta pots", rotation: -4, translate: 50, rotationSnippet: 6, translateSnippet: -70}, 
+  { key: 4, src: climbing, srcString: "/src/assets/Climbing.png", alt: "A rock climber scales a steep overhanging rock face as the sun beams behind her", rotation: 8, translate: -40, rotationSnippet: -2, translateSnippet: 40}, 
+  { key: 5, src: mountain, srcString: "/src/assets/Mountain.png", alt: "A snowy mountain peaks above the clouds", rotation: -12, translate: 60, rotationSnippet: 6, translateSnippet: -80}, 
 ]
 
 function Background() {
@@ -48,9 +48,41 @@ function ImageSpawnPoint() {
       style={{ transform: `rotate(${image.rotation}deg) translateX(${image.translate}px)` }}>
     </img>
   )
+
+  const codeStrings = [
+    "<",
+    "img",
+    "src=",
+    '"',
+    "alt=",
+    "</",
+    ">"
+  ]
+
+  const codeSnippets = images.map(image => 
+    <div className="snippet will-float" key={image.key} style={{ transform: `rotate(${image.rotationSnippet}deg) translateX(${image.translateSnippet}px)` }}>
+      <span className="grey">{codeStrings[0]}</span>
+      <span className="blue">{codeStrings[1]}</span>
+      <span> </span>
+      <span className="lightBlue">{codeStrings[2]}</span>
+      <span className="blue">{codeStrings[3]}</span>
+      <span className="blue">{image.srcString}</span>
+      <span className="blue">{codeStrings[3]}</span>
+      <span> </span>
+      <span className="lightBlue">{codeStrings[4]}</span>
+      <span className="orange">{codeStrings[3]}</span>
+      <span className="orange">{image.alt}</span>
+      <span className="orange">{codeStrings[3]}</span>
+      <span> </span>
+      <span className="grey">{codeStrings[5]}</span>
+      <span className="blue">{codeStrings[1]}</span>
+      <span className="grey">{codeStrings[6]}</span>
+    </div>
+  )
   return (
     <div className="spawn-point">
       {imageItems}
+      {codeSnippets}
     </div>
   )
 }
@@ -60,6 +92,13 @@ function imagesFloat() {
   images.forEach((image, index) => {
     setTimeout(() => {
       image.classList.add("float-up")
+    }, index * 5000)
+  })
+
+  const snippets = document.querySelectorAll(".snippet.will-float");
+  snippets.forEach((snippet, index) => {
+    setTimeout(() => {
+      snippet.classList.add("float-up")
     }, index * 5000)
   })
 }
