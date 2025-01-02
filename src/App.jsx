a01779be43be7ea17e2412b65947003c63bf2e2f
s111ew <167631312+s111ew@users.noreply.github.com>
2025-01-02 16:11:54 +0000

    Switch routing to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import './App.css'
-import Navbar from './Navbar.jsx'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeLayout from './HomeLayout.jsx'
-import DocsLayout from './DocsLayout.jsx'
+import './App.css';
+import Navbar from './Navbar.jsx';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeLayout from './HomeLayout.jsx';
+import DocsLayout from './DocsLayout.jsx';
 
 function App() {
   return (
-  <Router basename='/atg-landing-page'>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<HomeLayout />} />
-      <Route path='/docs' element={<DocsLayout />} />
-    </Routes>
-  </Router>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeLayout />} />
+        <Route path="/docs" element={<DocsLayout />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
